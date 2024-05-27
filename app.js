dcab4da9d21fc85d54814bec717a98c7f2696b16
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2024-05-27 22:02:28 +0200

    Adatbekérés 1-4. szekció

--- app.js
+++ app.js
@@ -1,6 +1,74 @@
+function RadioFigyelo(radiok){
+
+    if(radiok == "agyak")
+    {
+        let agyakradio = document.getElementsByName("agyak")
+        let agyszam;
+        for(let i = 0; i < agyakradio.length; i++)
+        {
+            if(agyakradio[i].checked)
+            {
+                agyszam = agyakradio[i].id
+            }
+        }
+        switch(agyszam)
+        {
+            case "egyagy":
+                return "Egyágyas - 9.000 Ft/éj";
+            case "ketagy":
+                return "Kétágyas - 15.000 Ft/éj";
+            case "ketegypot":
+                return "Kétágyas 1 pótággyal - 18.000 Ft/éj";
+            case "ketketpot":
+                return "Kétágyas 2 pótággyal - 21.000 Ft/éj";
+            default:
+                return "Nem választott ki szobatípust!";
+        }
+    }
+    else if(radiok == "ellatas")
+    {
+        let ellatasradio = document.getElementsByName("ellatasv")
+        let ellatastipus;
+        for(let i = 0; i < ellatasradio.length; i++)
+        {
+            if(ellatasradio[i].checked)
+            {
+                ellatastipus = ellatasradio[i].id
+            }
+        }
+        switch(ellatastipus)
+        {
+            case "reggeli":
+                return "Reggeli - 900 Ft/nap/fő";
+            case "felpanz":
+                return "Félpanzió - 2.900 Ft/nap/fő";
+            case "teljespanz":
+                return "Teljes panzíó - 4.900 Ft/nap/fő";
+            default:
+                return "Nem adott meg ellátást!"
+        }
+
+    }
+}
+
+function CheckBoxok()
+{
+
+}
+
 function Adatbekeres()
 {
     adatok = [
-        document
+        document.getElementById("erkezes").value,
+        document.getElementById("tavozas").value,
+        RadioFigyelo("agyak"),
+        document.getElementById("fovalaszto").value,
+        document.getElementById("fo1").value,
+        document.getElementById("fo2").value,
+        document.getElementById("fo3").value,
+        document.getElementById("fo4").value,
+        RadioFigyelo("ellatas"),
+        CheckBoxok()
     ]
+    alert(adatok)
 };
